d2fe9d55c5179789b4f61af9629a602e81832489
zinzuvadiya Meet <zinzuvadiyameet98@gmail.com>
2021-10-11 10:46:33 +0530

    login flow done || find by id working properly

--- modules/userModule.js
+++ modules/userModule.js
@@ -1,4 +1,4 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     uName : {type:String, required:true},
--- routes/auth.js
+++ routes/auth.js
@@ -1,90 +1,107 @@
-const router = require('express').Router();
-const User = require('../modules/userModule');
+const router = require("express").Router();
+const User = require("../modules/userModule");
 var CryptoJS = require("crypto-js");
 
-router.get("/",async(req,res)=>{
+router.get("/", async (req, res) => {
+  res.json({
+    message: "Welcome to blackdevil auth api",
+    routes: {
+      "/registration": "new user registration",
+      "/login": "user login",
+      "/info/:id": "get user data",
+    },
+  });
+});
+
+router.post("/registration", async (req, res) => {
+  const otp = Math.floor(100000 + Math.random() * 900000);
+  const newUser = new User({
+    uName: req.body.uName,
+    uAddress: req.body.uAddress,
+    uEmail: req.body.uEmail,
+    uPassword: req.body.uPassword,
+    uNumber: req.body.uNumber,
+    uOTP: otp,
+  });
+  try {
+    const userData = await newUser.save();
     res.json({
-        "message": "Welcome to blackdevil auth api"
+      status: 201,
+      message: "user registrated",
+      data: {
+        uId: userData._id.toString(),
+        name: userData.uName,
+        address: userData.uAddress,
+        email: userData.uEmail,
+        phone: userData.uNumber,
+        otp: userData.uOTP,
+      },
     });
-})
-
-router.post("/registration",async(req,res)=>{
-    const otp = Math.floor(100000 + Math.random() * 900000);
-    const newUser = new User({
-        uName :req.body.uName,
-        uAddress :req.body.uAddress,
-        uEmail :req.body.uEmail,
-        uPassword : CryptoJS.AES.encrypt(req.body.uPassword, process.env.passKey).toString(),
-        uNumber :req.body.uNumber,
-        uOTP : otp,
-    })
-    try {
-        const userData = await newUser.save();
-        res.json({
-            "status": 201,
-            "message":"user registrated",
-            "data":{
-                "uId":userData._id.toString(),
-                "name":userData.uName,
-                "address":userData.uAddress,
-                "email":userData.uEmail,
-                "phone":userData.uNumber,
-                "otp":userData.uOTP,
-            }
-        });
-    } catch (err) {
-        console.error(err);
-        res.json({
-            "status": 500,
-            "message":"error occur",
-            "error": err.message
-        })
-    }
+  } catch (err) {
+    console.error(err);
+    res.json({
+      status: 500,
+      message: "eror occrur",
+      error: "User already registrated",
+    });
+  }
 });
 
-router.post("/login",async(req,res)=>{
-    try {
-
-        const user = User.find({ uEmail: req.body.uEmail });
-        !user && res.status(500).json({
-            "status":500,
-            "message" : "user not found"
-        });
-
-        // const orgPassword = CryptoJS.AES.decrypt(user.uPassword, process.env.passKey);
-        // const pwd = orgPassword.toString();
-
-        console.log("Password : ",user);
-        
-        // pwd !== req.body.uPassword && 
-        // res.status(500).json({
-        //     "status":500,
-        //     "message" : "password wrong"
-        // });
-
-        // res.status(200).json({
-        //     "status":200,
-        //     "data":user
-        // })
-        
-    } catch (err) {
-        console.error(err);
-        res.json({
-            "status": 500,
-            "message":"error occur",
-            "error": err.message
-        })
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ uEmail: req.body.uEmail });
+    if (!user) {
+      res.status(404).json({ status: 404, message: "user not found" });
+    } else if (user.uPassword !== req.body.uPassword) {
+      res.status(401).json({ status: 401, message: "password wrong" });
+    } else {
+      res.status(200).json({
+        status: 200,
+        message: "user loged in",
+        data: {
+          uId: user._id.toString(),
+          name: user.uName,
+          email: user.uEmail,
+          number: user.uNumber,
+          address: user.uAddress,
+        },
+      });
     }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      status: 500,
+      message: err.message,
+    });
+  }
 });
 
-router.get("/info/:id", async(req,res)=>{
-    const { id } = req.params;
-    try {
-        
-    } catch (err) {
-        console.error(err.message);
+router.get("/info/:id", async (req, res) => {
+  try {
+    const uId = req.params.id;
+    const user = await User.findOne({ _id: uId });
+    if (!user) {
+      res.status(404).json({ status: 404, message: "user not found" });
+    } else {
+      res.status(200).json({
+        status: 200,
+        message: "user's details",
+        data: {
+          uId: user._id.toString(),
+          name: user.uName,
+          email: user.uEmail,
+          number: user.uNumber,
+          address: user.uAddress,
+        },
+      });
     }
-
-})
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      status: 500,
+      message: err.message,
+    });
+  }
+});
 
 module.exports = router;
